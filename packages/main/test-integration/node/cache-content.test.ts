aa32b7856cac69b3b08a47191ee44f2832d8a137
DellaBitta <drsanta@google.com>
2024-06-12 15:20:53 -0400

    Add displayName to iTests. Format.

--- packages/main/test-integration/node/cache-content.test.ts
+++ packages/main/test-integration/node/cache-content.test.ts
@@ -18,7 +18,7 @@
 import { expect, use } from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import { GoogleGenerativeAI } from "../..";
-import { GoogleAICacheManager }  from "../../server"; //= require("@google/generative-ai/server");
+import { GoogleAICacheManager } from "../../server"; //= require("@google/generative-ai/server");
 
 use(chaiAsPromised);
 
@@ -29,102 +29,139 @@ use(chaiAsPromised);
 describe("cacheContent", function () {
   this.timeout(60e3);
   this.slow(10e3);
-  let text :string = '';
-  const endpointConfiguration = {
-  };
+  let text: string = "";
+  const endpointConfiguration = { /* remove at launch */ };
 
-  for (let i = 0; i < 32001; i++) {
+  for (let i = 0; i < 1500; i++) {
     text += "Purple cats drink gatorade.";
-    text += i % 8 === 7 ? '\n' : ' ';
+    text += i % 8 === 7 ? "\n" : " ";
   }
   it("createCache", async () => {
     // cacheManager create
     const ttlSeconds = 5;
-    const cacheManager = new GoogleAICacheManager(process.env.GEMINI_API_KEY || "", endpointConfiguration);
+    const displayName = "A display name.";
+    const cacheManager = new GoogleAICacheManager(
+      process.env.GEMINI_API_KEY || "",
+      endpointConfiguration,
+    );
     const createCacheResult = await cacheManager.create({
       ttlSeconds,
       model: "models/gemini-1.5-pro",
-      contents: [{
-        role: 'user',
-        parts: [{ text }]
-      }]
+      contents: [
+        {
+          role: "user",
+          parts: [{ text }],
+        },
+      ],
+      displayName,
     });
     expect(createCacheResult.name).to.exist;
     expect(createCacheResult.model).to.exist;
     expect(createCacheResult.createTime).to.exist;
     expect(createCacheResult.updateTime).to.exist;
     expect(createCacheResult.expireTime).to.exist;
-    expect(createCacheResult.name.startsWith('cachedContents/')).to.be.true;
+    expect(createCacheResult.displayName).to.exist.and.equal(displayName);
+    expect(createCacheResult.name.startsWith("cachedContents/")).to.be.true;
     const createdTime = Date.parse(createCacheResult.createTime);
     const expireTime = Date.parse(createCacheResult.expireTime);
-    expect(expireTime - createdTime).to.be.lessThanOrEqual(ttlSeconds*1000);
+    expect(expireTime - createdTime).to.be.lessThanOrEqual(ttlSeconds * 1000);
   });
   it("cacheManager list", async () => {
     // cacheManager create
-    const cacheManager = new GoogleAICacheManager(process.env.GEMINI_API_KEY || "", endpointConfiguration);
+    const displayName = new Date().toISOString();
+    const cacheManager = new GoogleAICacheManager(
+      process.env.GEMINI_API_KEY || "",
+      endpointConfiguration,
+    );
     const createCacheResult = await cacheManager.create({
       ttlSeconds: 5,
       model: "models/gemini-1.5-pro",
-      contents: [{
-        role: 'user',
-        parts: [{ text }]
-      }]
+      contents: [
+        {
+          role: "user",
+          parts: [{ text }],
+        },
+      ],
+      displayName,
     });
     expect(createCacheResult.name).to.exist;
-    const cacheName = createCacheResult.name;
-    expect(cacheName.startsWith('cachedContents/')).to.be.true;
-  
+    expect(createCacheResult.displayName).to.exist.and.equal(displayName);
+    expect(createCacheResult.name.startsWith("cachedContents/")).to.be.true;
+
     // List
     const listResult = await cacheManager.list();
     expect(listResult.cachedContents).to.exist;
-    expect(listResult.cachedContents.map(e=>({name: e.name}))).to.deep.include({ name: cacheName} );
+    expect(
+      listResult.cachedContents.map((e) => ({ name: e.name })),
+    ).to.deep.include({ name: createCacheResult.name });
+    expect(
+      listResult.cachedContents.map((e) => ({ displayName: e.displayName })),
+    ).to.deep.include({ displayName: displayName });
   });
   it("cacheManager get", async () => {
     // cacheManager create
-    const cacheManager = new GoogleAICacheManager(process.env.GEMINI_API_KEY || "", endpointConfiguration);
+    const displayName = new Date().toISOString();
+    const cacheManager = new GoogleAICacheManager(
+      process.env.GEMINI_API_KEY || "",
+      endpointConfiguration,
+    );
     const createCacheResult = await cacheManager.create({
       ttlSeconds: 5,
       model: "models/gemini-1.5-pro",
-      contents: [{
-        role: 'user',
-        parts: [{ text }]
-      }]
+      contents: [
+        {
+          role: "user",
+          parts: [{ text }],
+        },
+      ],
+      displayName,
     });
     expect(createCacheResult.name).to.exist;
-    const cacheName = createCacheResult.name;
-    expect(cacheName.startsWith('cachedContents/')).to.be.true;
-  
+    expect(createCacheResult.displayName).to.exist.and.equal(displayName);
+    expect(createCacheResult.name.startsWith("cachedContents/")).to.be.true;
+
     // cacheManager.get
-    const cache = await cacheManager.get(cacheName);
+    const cache = await cacheManager.get(createCacheResult.name);
     expect(cache.name).to.equal(createCacheResult.name);
     expect(cache.model).to.equal(createCacheResult.model);
     expect(cache.createTime).to.equal(createCacheResult.createTime);
     expect(cache.updateTime).to.equal(createCacheResult.updateTime);
     expect(cache.expireTime).to.equal(createCacheResult.expireTime);
+    expect(cache.displayName).to.exist.and.equal(displayName);
   });
   it("cacheManager update ttl then get", async () => {
     // cacheManager create
+    const displayName = new Date().toISOString();
     const originalTtlSeconds = 20;
-    const cacheManager = new GoogleAICacheManager(process.env.GEMINI_API_KEY || "", endpointConfiguration);
+    const cacheManager = new GoogleAICacheManager(
+      process.env.GEMINI_API_KEY || "",
+      endpointConfiguration,
+    );
     const createCacheResult = await cacheManager.create({
       ttlSeconds: originalTtlSeconds,
       model: "models/gemini-1.5-pro",
-      contents: [{
-        role: 'user',
-        parts: [{ text }]
-      }]
+      contents: [
+        {
+          role: "user",
+          parts: [{ text }],
+        },
+      ],
+      displayName,
     });
     expect(createCacheResult.name).to.exist;
-    const cacheName = createCacheResult.name;
-    expect(cacheName.startsWith('cachedContents/')).to.be.true;
-  
+    expect(createCacheResult.name.startsWith("cachedContents/")).to.be.true;
+    expect(createCacheResult.displayName).to.exist.and.equal(displayName);
+
     // cacheManager.update
     const newTtlSeconds = originalTtlSeconds + 10;
     const updateParams = { cachedContent: { ttlSeconds: newTtlSeconds } };
-    const updateResult = await cacheManager.update(createCacheResult.name, updateParams);
+    const updateResult = await cacheManager.update(
+      createCacheResult.name,
+      updateParams,
+    );
 
     // cacheManager.get
-    const cache = await cacheManager.get(cacheName);
+    const cache = await cacheManager.get(createCacheResult.name);
     expect(cache.name).to.equal(createCacheResult.name);
     expect(cache.name).to.equal(updateResult.name);
     expect(cache.model).to.equal(createCacheResult.model);
@@ -134,34 +171,49 @@ describe("cacheContent", function () {
     expect(cache.updateTime).to.equal(updateResult.updateTime);
     expect(cache.expireTime).to.not.equal(createCacheResult.expireTime);
     expect(cache.expireTime).to.equal(updateResult.expireTime);
+    expect(cache.displayName).to.exist.and.equal(displayName);
     const createdTime = Date.parse(createCacheResult.createTime);
     const expireTime = Date.parse(createCacheResult.expireTime);
     const updatedTime = Date.parse(cache.updateTime);
-    expect(expireTime - createdTime).to.be.lessThanOrEqual(originalTtlSeconds*1000);
-    expect(expireTime - updatedTime).to.be.lessThanOrEqual(newTtlSeconds*1000);
+    expect(expireTime - createdTime).to.be.lessThanOrEqual(
+      originalTtlSeconds * 1000,
+    );
+    expect(expireTime - updatedTime).to.be.lessThanOrEqual(
+      newTtlSeconds * 1000,
+    );
   });
   it("cacheManager update expireTime then get", async () => {
     // cacheManager create
-    const cacheManager = new GoogleAICacheManager(process.env.GEMINI_API_KEY || "", endpointConfiguration);
+    const cacheManager = new GoogleAICacheManager(
+      process.env.GEMINI_API_KEY || "",
+      endpointConfiguration,
+    );
     const createCacheResult = await cacheManager.create({
       ttlSeconds: 20,
       model: "models/gemini-1.5-pro",
-      contents: [{
-        role: 'user',
-        parts: [{ text }]
-      }]
+      contents: [
+        {
+          role: "user",
+          parts: [{ text }],
+        },
+      ],
     });
     expect(createCacheResult.name).to.exist;
-    const cacheName = createCacheResult.name;
-    expect(cacheName.startsWith('cachedContents/')).to.be.true;
-  
+    expect(createCacheResult.name.startsWith("cachedContents/")).to.be.true;
+    expect(createCacheResult.displayName).to.equal("");
+
     // cacheManager.update
-    const newExpirationTime = new Date( new Date().getTime() + 30*1000);
-    const updateParams = { cachedContent: { expireTime: newExpirationTime.toISOString() } };
-    const updateResult = await cacheManager.update(createCacheResult.name, updateParams);
+    const newExpirationTime = new Date(new Date().getTime() + 30 * 1000);
+    const updateParams = {
+      cachedContent: { expireTime: newExpirationTime.toISOString() },
+    };
+    const updateResult = await cacheManager.update(
+      createCacheResult.name,
+      updateParams,
+    );
 
     // cacheManager.get
-    const cache = await cacheManager.get(cacheName);
+    const cache = await cacheManager.get(createCacheResult.name);
     expect(cache.name).to.equal(createCacheResult.name);
     expect(cache.name).to.equal(updateResult.name);
     expect(cache.model).to.equal(createCacheResult.model);
@@ -173,25 +225,33 @@ describe("cacheContent", function () {
     expect(cache.expireTime).to.equal(updateResult.expireTime);
   });
   it("generateContentWithCache", async () => {
-    const cacheManager = new GoogleAICacheManager(process.env.GEMINI_API_KEY || "", endpointConfiguration);
+    const cacheManager = new GoogleAICacheManager(
+      process.env.GEMINI_API_KEY || "",
+      endpointConfiguration,
+    );
     const createCacheResult = await cacheManager.create({
       ttlSeconds: 20,
       model: "models/gemini-1.5-pro",
-      contents: [{
-        role: 'user',
-        parts: [{ text }]
-      }]
+      contents: [
+        {
+          role: "user",
+          parts: [{ text }],
+        },
+      ],
     });
     expect(createCacheResult.name).to.exist;
     const cacheName = createCacheResult.name;
-    expect(cacheName.startsWith('cachedContents/')).to.be.true;
-  
+    expect(cacheName.startsWith("cachedContents/")).to.be.true;
+
     // cacheManager.get
     const cache = await cacheManager.get(cacheName);
 
     // generate content.
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModelFromCachedContent(cache, endpointConfiguration);
+    const model = genAI.getGenerativeModelFromCachedContent(
+      cache,
+      endpointConfiguration,
+    );
     const result = await model.generateContent({
       contents: [
         {
