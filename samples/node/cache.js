ec948ffa91cae2a314a4dd15fa571003209a015a
Christina Holland <chholland@google.com>
2024-07-10 14:47:16 -0700

    add cached content samples

--- samples/node/cache.js
+++ samples/node/cache.js
@@ -16,7 +16,10 @@
  */
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GoogleAICacheManager, GoogleAIFileManager } from "@google/generative-ai/server";
+import {
+  GoogleAICacheManager,
+  GoogleAIFileManager,
+} from "@google/generative-ai/server";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -28,17 +31,23 @@ async function cacheCreate() {
   const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
   const fileManager = new GoogleAIFileManager(process.env.API_KEY);
 
-  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, { mimeType: 'text/plain' });
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
 
   const cacheResult = await cacheManager.create({
     model: "models/gemini-1.5-flash-001",
     contents: [
       {
         role: "user",
-        parts: [{ fileData: {
-          fileUri: uploadResult.file.uri,
-          mimeType: uploadResult.file.mimeType
-        } }],
+        parts: [
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ],
       },
     ],
   });
@@ -47,10 +56,12 @@ async function cacheCreate() {
 
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModelFromCachedContent(cacheResult);
-  const result = await model.generateContent("Please summarize this transcript.");
+  const result = await model.generateContent(
+    "Please summarize this transcript.",
+  );
   console.log(result.response.text());
-  await cacheManager.delete(cacheResult.name);
   // [END cache_create]
+  await cacheManager.delete(cacheResult.name);
 }
 
 async function cacheCreateFromName() {
@@ -58,17 +69,23 @@ async function cacheCreateFromName() {
   const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
   const fileManager = new GoogleAIFileManager(process.env.API_KEY);
 
-  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, { mimeType: 'text/plain' });
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
 
   const cacheResult = await cacheManager.create({
     model: "models/gemini-1.5-flash-001",
     contents: [
       {
         role: "user",
-        parts: [{ fileData: {
-          fileUri: uploadResult.file.uri,
-          mimeType: uploadResult.file.mimeType
-        } }],
+        parts: [
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ],
       },
     ],
   });
@@ -79,14 +96,195 @@ async function cacheCreateFromName() {
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModelFromCachedContent(getCacheResult);
   model.generateContent("Please summarize this transcript.");
-  await cacheManager.delete(cacheResult.name);
   // [END cache_create_from_name]
+  await cacheManager.delete(cacheResult.name);
+}
+
+async function cacheCreateFromChat() {
+  // [START cache_create_from_chat]
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
+  const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-001" });
+  const chat = model.startChat();
+
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
+
+  let result = await chat.sendMessage([
+    "Hi, could you summarize this transcript?",
+    {
+      fileData: {
+        fileUri: uploadResult.file.uri,
+        mimeType: uploadResult.file.mimeType,
+      },
+    },
+  ]);
+  console.log(`\n\nmodel: ${result.response.text()}`);
+  result = await chat.sendMessage(
+    "Okay, could you tell me more about the trans-lunar injection",
+  );
+  console.log(`\n\nmodel: ${result.response.text()}`);
+
+  const cacheResult = await cacheManager.create({
+    model: "models/gemini-1.5-flash-001",
+    contents: await chat.getHistory(),
+  });
+
+  const newModel = genAI.getGenerativeModelFromCachedContent(cacheResult);
+
+  const newChat = newModel.startChat();
+  result = await newChat.sendMessage(
+    "I didn't understand that last part, could you explain it in simpler language?",
+  );
+  console.log(`\n\nmodel: ${result.response.text()}`);
+  // [END cache_create_from_chat]
+
+  await cacheManager.delete(cacheResult.name);
+}
+
+async function cacheDelete() {
+  // [START cache_delete]
+  const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
+  const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
+
+  const cacheResult = await cacheManager.create({
+    model: "models/gemini-1.5-flash-001",
+    contents: [
+      {
+        role: "user",
+        parts: [
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ],
+      },
+    ],
+  });
+  await cacheManager.delete(cacheResult.name);
+  // [END cache_delete]
+}
+
+async function cacheGet() {
+  // [START cache_get]
+  const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
+  const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
+
+  const cacheResult = await cacheManager.create({
+    model: "models/gemini-1.5-flash-001",
+    contents: [
+      {
+        role: "user",
+        parts: [
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ],
+      },
+    ],
+  });
+  const cacheGetResult = await cacheManager.get(cacheResult.name);
+  console.log(cacheGetResult);
+  // [END cache_get]
+  await cacheManager.delete(cacheResult.name);
+}
+
+async function cacheList() {
+  // [START cache_list]
+  const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
+  const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
+
+  const cacheResult = await cacheManager.create({
+    model: "models/gemini-1.5-flash-001",
+    contents: [
+      {
+        role: "user",
+        parts: [
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ],
+      },
+    ],
+  });
+  console.log("My caches:");
+  const cacheListResult = await cacheManager.list();
+  for (const item of cacheListResult.cachedContents) {
+    console.log(item);
+  }
+  // [END cache_list]
+  await cacheManager.delete(cacheResult.name);
+}
+
+async function cacheUpdate() {
+  // [START cache_update]
+  const cacheManager = new GoogleAICacheManager(process.env.API_KEY);
+  const fileManager = new GoogleAIFileManager(process.env.API_KEY);
+
+  const uploadResult = await fileManager.uploadFile(`${mediaPath}/a11.txt`, {
+    mimeType: "text/plain",
+  });
+
+  const cacheResult = await cacheManager.create({
+    model: "models/gemini-1.5-flash-001",
+    contents: [
+      {
+        role: "user",
+        parts: [
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ],
+      },
+    ],
+  });
+  console.log("initial cache data:", cacheResult);
+  const cacheUpdateResult = await cacheManager.update(cacheResult.name, {
+    cachedContent: {
+      // 2 hours
+      ttlSeconds: 60 * 60 * 2,
+    },
+  });
+  console.log("updated cache data:", cacheUpdateResult);
+  // [END cache_update]
+  await cacheManager.delete(cacheResult.name);
 }
 
 async function runAll() {
   // Comment out or delete any sample cases you don't want to run.
   await cacheCreate();
-  await cacheCreateFromName
+  await cacheCreateFromName();
+  await cacheCreateFromChat();
+  await cacheDelete();
+  await cacheGet();
+  await cacheList();
+  await cacheUpdate();
 }
 
 runAll();
