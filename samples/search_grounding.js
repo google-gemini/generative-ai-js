946639952da3fafe582d68191899647fca269ed7
Sixia "Leask" Huang <i@leaskh.com>
2025-02-21 00:14:13 -0500

    Adding google search tool sample (#318)
    * Add a sample for google_search tool with gemini 2.0 models

--- samples/search_grounding.js
+++ samples/search_grounding.js
@@ -20,6 +20,7 @@ import {
   GoogleGenerativeAI,
 } from "@google/generative-ai";
 
+// `google_search_retrieval` is available in Gemini 1.5
 async function searchGrounding() {
   // [START search_grounding]
   // Make sure to include these imports:
@@ -50,9 +51,36 @@ async function searchGrounding() {
   console.log(result.response.candidates[0].groundingMetadata);
   // [END search_grounding]
 }
+
+// `google_search` is available in Gemini 2.0
+async function googleSearch() {
+  // [START google_search]
+  // Make sure to include these imports:
+  // import {
+  //  GoogleGenerativeAI,
+  // } from "@google/generative-ai";
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  const model = genAI.getGenerativeModel(
+    {
+      model: "gemini-2.0-flash-exp",
+      tools: [
+        {
+          googleSearch: {},
+        },
+      ],
+    },
+  );
+
+  const prompt = "What is the price of Google stock today?";
+  const result = await model.generateContent(prompt);
+  console.log(result.response.candidates[0].groundingMetadata);
+  // [END google_search]
+}
+
 async function runAll() {
   // Comment out or delete any sample cases you don't want to run.
   await searchGrounding();
+  await googleSearch();
 }
 
 runAll();
